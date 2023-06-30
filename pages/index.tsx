3e5f420c319933cc7529eb9f4ebbcaed676710f3
gift56 <efeasiughu@gmail.com>
2023-06-30 06:22:17 -0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      {/* <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      /> */}
+      Homepage
+      
+    </div>
+  );
+};
+
+export default Homepage;
