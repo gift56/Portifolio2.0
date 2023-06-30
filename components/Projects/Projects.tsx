9d811b2a91df79d214e6313b6991ca3b8da53979
gift56 <efeasiughu@gmail.com>
2023-06-30 10:59:08 -0700

    working on project

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -7,6 +7,51 @@ import "swiper/css/navigation";
 import Link from "next/link";
 
 const Projects = () => {
+  const data = [
+    {
+      image: "/img/image1.png",
+      title: "Tiktik-World",
+      desc: "A dynamic and engaging environment for users to create, discover, and share short videos.",
+      liveUrl: "https://tiktik-world.vercel.app",
+      repo: "https://github.com/gift56/Tiktik_World",
+    },
+    {
+      image: "/img/image2.png",
+      title: "Zfarmers",
+      desc: "A dynamic website that Empowering farmers through blockchain. Start farming, earn, travel, play and learn with zkFarmers.",
+      liveUrl: "https://zkfarmers.io/",
+      repo: "https://github.com/ShopOasisDapp/zkBuds-Connect",
+    },
+    {
+      image: "/img/image3.png",
+      title: "Ivorytusk",
+      desc: "A dynamic website that promote the adoption of blockchain technology and support the growth of the web3 ecosystem.",
+      liveUrl: "https://ivorytusk.finance/",
+      repo: "https://ivorytusk.finance/",
+    },
+    {
+      image: "/img/image4.png",
+      title: "MovieWorld",
+      desc: "A dynamic website that promote the adoption of blockchain technology and support the growth of the web3 ecosystem.",
+      liveUrl: "https://netflix-clone-roan-nine.vercel.app/",
+      repo: "https://github.com/gift56/Netflix_Clone",
+    },
+    {
+      image: "/img/image5.png",
+      title: "MovieWorld",
+      desc: "A dynamic website that provides the opportunity to Find the perfect freelance services for your business",
+      liveUrl: "https://fiverr-web.netlify.app/",
+      repo: "https://github.com/gift56/Fiverr-clone",
+    },
+    {
+      image: "/img/image6.png",
+      title: "MovieWorld",
+      desc: "A website that promotes Grow with Innovative Product Development, High-End Web Design, and Effective Branding.",
+      liveUrl: "https://colhacklabs.com/",
+      repo: "https://colhacklabs.com/",
+    },
+  ];
+
   const [screen, setScreen] = useState<number | any>(null);
 
   useEffect(() => {
@@ -47,9 +92,7 @@ const Projects = () => {
                 clickable: true,
               }}
               modules={[Autoplay, Pagination]}
-            >
-                
-            </Swiper>
+            ></Swiper>
           </div>
         </div>
       </div>
