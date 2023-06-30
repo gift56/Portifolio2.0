44429fc58afe32753f147e61d2f737bd5ffdf406
gift56 <efeasiughu@gmail.com>
2023-06-30 16:49:59 -0700

    working on footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="w-full py-10 bg-[#1C1C65]">
+      <div className="container">
+        <div className="flex items-center flex-col justify-center w-full">
+          <div className="w-full flex flex-wrap lg:flex-nowrap gap-8 items-center justify-center">
+            
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
