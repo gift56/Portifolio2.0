f33d7bd4ff33d0d0d18b0421a128d858167f4203
gift56 <efeasiughu@gmail.com>
2023-06-30 07:04:02 -0700

    working on layout

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link as ScrollLink } from "react-scroll";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className="w-full flex  items-center justify-center py-4 gap-8">
+    <ScrollLink></ScrollLink>
+  </header>;
 };
 
 export default Header;
