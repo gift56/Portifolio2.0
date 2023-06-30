e90ad8855c91f1e6d3b2c145549903a39b1ffbcb
gift56 <efeasiughu@gmail.com>
2023-06-30 07:05:10 -0700

    working on header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { Link as ScrollLink } from "react-scroll";
 
 const Header = () => {
-  return <header className="w-full flex  items-center justify-center py-4 gap-8">
-    <ScrollLink></ScrollLink>
-  </header>;
+  return (
+    <header className="w-full flex  items-center justify-center py-4 gap-8">
+      <ScrollLink
+        to="mining"
+        spy={true}
+        smooth={true}
+        className="flex items-center gap-2 font-general text-lg font-normal text-dark cursor-pointer hover:border-b-[3px] hover:border-success h-full"
+      >Home</ScrollLink>
+    </header>
+  );
 };
 
 export default Header;
