4c93bef409adece2ef6c5d9ea618d065a38358d8
gift56 <efeasiughu@gmail.com>
2023-06-30 09:36:41 -0700

    working on about page

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -3,7 +3,19 @@ import React from "react";
 const About = () => {
   return (
     <section className="py-10 w-full">
-      <div className="container">sdn</div>
+      <div className="container">
+        <div className="flex flex-col items-start justify-start w-full gap-3">
+          <h2 className="text-2xl md:text-3xl font-semibold text-blue-500">
+            About Me
+          </h2>
+          <p className="text-sm md:text-base font-medium">
+            Hi, my name is Efe Asieghwu Gift, i am a Frontend web developer,
+            React developer, and a Tech influencer. I have honed my skills in
+            Web Development and advance i have core understanding of advance UI
+            design principles. Here are the major skills i do dominate.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
