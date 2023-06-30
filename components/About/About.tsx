7561233f56ab9e967085c0e1728def22d3f420e8
gift56 <efeasiughu@gmail.com>
2023-06-30 09:58:14 -0700

    working on about page

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import { FaReact, FaLaptopCode } from "react-icons/fa";
+import { TbBrandJavascript } from "react-icons/tb";
+import { TfiRulerPencil } from "react-icons/tfi";
 
 const About = () => {
+  const data = [
+    {
+      icon: TfiRulerPencil,
+      title: "UI & UX DESIGNING",
+    },
+    {
+      icon: FaLaptopCode,
+      title: "WEB DEVELOPMENT",
+    },
+    {
+      icon: TbBrandJavascript,
+      title: "JAVASCRIPT DEVELOPMENT",
+    },
+    {
+      icon: FaReact,
+      title: "REACT DEVELOPMENT",
+    },
+  ];
   return (
     <section className="py-10 w-full">
       <div className="container">
@@ -19,10 +40,23 @@ const About = () => {
               4+
             </span>
             <span className="text-lg md:text-xl font-medium">
-              Years of experience. Specialised in building web applications, while ensuring
-              a seamless
-              <br className="hidden lg:block" /> web performance and user experience.
+              Years of experience. Specialised in building web applications,
+              while ensuring a seamless
+              <br className="hidden lg:block" /> web performance and user
+              experience.
             </span>
+          </div>
+          <div className="w-full mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+            {data.map((item) => (
+              <div
+                key={item.title}
+                className="w-full h-[254px] bg-slate-700 transition-all duration-300 hover:bg-secondary rounded flex flex-col items-start justify-end p-4"
+              >
+                <span>
+                  <item.icon />
+                </span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
