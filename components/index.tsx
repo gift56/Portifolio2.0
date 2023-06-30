ef3b9b5e66105e8f86876f31ad88b60fc9ada7ea
gift56 <efeasiughu@gmail.com>
2023-06-30 09:28:17 -0700

    working on about page

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,7 @@
+import About from "./About/About";
 import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Hero from "./Hero/Hero";
 
-export { Header, Footer, Hero, Button };
+export { Header, Footer, Hero, Button, About };
