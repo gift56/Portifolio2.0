87147171b63cac9a05981ad7ae21a4d32334ddae
gift56 <efeasiughu@gmail.com>
2023-06-30 11:32:54 -0700

    working on contact section

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -4,7 +4,19 @@ const Contact = () => {
   return (
     <section id="contact" className="py-10 w-full">
       <div className="container">
-        <div></div>
+        <div className="w-full flex flex-col items-start justify-start tab:flex-row gap-10">
+          <div className="tab:flex-1 w-full flex flex-col items-start justify-start gap-4">
+            <h2 className="text-2xl font-semibold md:text-3xl text-secondary">
+              Get in touch
+            </h2>
+            <p className="text-sm md:text-base font-normal">
+              I’m very approachable and would love to speak to you. Feel free to
+              call, send me an email . Follow me in social media or simply
+              complete the enquiry form.
+            </p>
+          </div>
+          <div className="tab:flex-1 w-full flex flex-col items-start justify-start gap-4"></div>
+        </div>
       </div>
     </section>
   );
