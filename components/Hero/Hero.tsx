6c904796d9d5a744388cf80268cef5e91a762f5f
gift56 <efeasiughu@gmail.com>
2023-06-30 07:34:32 -0700

    workingon hero page

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Hero = () => {
-  return <section className="">Hero</section>;
+  return (
+    <section className="py-10 w-full">
+      <div className="container">
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
