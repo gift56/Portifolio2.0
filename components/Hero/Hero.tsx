0f6bb4d4c7921dedbc9a78a02044cabf1a0d9e5f
gift56 <efeasiughu@gmail.com>
2023-06-30 09:08:41 -0700

    working on hero page

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,9 +3,9 @@ import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   return (
-    <section className="py-10 w-full xl:h-[80vh] xl:pt-0">
+    <section className="py-10 w-full xl:h-[90vh] xl:pt-0">
       <div className="container h-full">
-        <div className="w-full flex flex-col items-end justify-center tab:flex-row gap-6 h-full">
+        <div className="w-full flex flex-col items-center justify-center tab:flex-row gap-10 h-full">
           <div className="tab:flex-1 w-full flex flex-col items-start justify-start gap-4">
             <h1 className="text-2xl font-semibold md:text-3xl">
               Hello, my name is{" "}
@@ -26,18 +26,19 @@ const Hero = () => {
               className="text-2xl font-semibold md:text-3xl"
               repeat={Infinity}
             />
-            <span>
-              Freelance UI designer, Fullstack developer, & Data Miner. I create
-              seamless web experiences for end-users.
+            <span className="text-base md:text-lg font-medium">
+              Freelance Software Engineer, Fullstack developer, & Data Miner. I
+              create seamless web experiences the meets client expectation.
             </span>
           </div>
-          <div className="tab:flex-1 w-full relative flex items-center justify-center h-full">
-            <img
-              src="/img/heroImg.png"
-              alt="hero_image"
-              className="relative bottom-10 z-10"
-            />
-            <div className="absolute w-[80%] left-8 bg-[#fa5b0f] bottom-0 h-[70%] rounded-[50%]"></div>
+          <div className="tab:flex-1 w-full relative flex items-center justify-center h-full md:overflow-hidden">
+            <div className="w-full bg-[#FA5B0F] md:absolute h-[416px] rounded-full flex items-center justify-center overflow-hidden">
+              <img
+                src="/img/heroImg.png"
+                alt="hero_image"
+                className="w-[418px] h-[416px] rounded-lg relative z-10 object-contain"
+              />
+            </div>
           </div>
         </div>
       </div>
