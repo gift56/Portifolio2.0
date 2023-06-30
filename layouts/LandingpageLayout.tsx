bca92bb3100c877c8fca994ac7731de428f4b6e6
gift56 <efeasiughu@gmail.com>
2023-06-30 07:01:30 -0700

    working on layout

--- layouts/LandingpageLayout.tsx
+++ layouts/LandingpageLayout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
+import { Footer, Header } from "@/components";
 
 interface MainLayoutProps {
   pageTitle?: string;
@@ -18,7 +19,9 @@ const LandingpageLayout = ({ pageTitle, children }: MainLayoutProps) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="img/logo.svg" sizes="1088x468" />
       </Head>
-      {children}
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,7 @@ import LandingpageLayout from "@/layouts/LandingpageLayout";
 const Homepage = () => {
   return (
     <LandingpageLayout pageTitle="I create seamless web experiences for end-users">
-      {/* <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        className="dark:invert"
-        width={100}
-        height={24}
-        priority
-      /> */}
-      Homepage
+     
     </LandingpageLayout>
   );
 };
