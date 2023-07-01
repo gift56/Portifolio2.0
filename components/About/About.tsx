e522b8b52757ac767a5392f9a534e1cb88423235
gift56 <efeasiughu@gmail.com>
2023-07-01 06:05:23 -0700

    adding some animation

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -42,29 +42,78 @@ const About = () => {
       ),
     },
   ];
+
+  const container = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.4,
+        duration: 0.3,
+        type: "spring",
+      },
+    },
+  };
+
+  const itemdisplay = {
+    hidden: { y: 60, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      type: "spring",
+    },
+  };
+
   return (
     <section id="about" className="py-10 w-full">
       <div className="container">
         <div className="flex flex-col items-start justify-start w-full gap-3">
-          <h2 className="text-2xl md:text-3xl font-semibold text-blue-500">
+          <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ type: "spring", duration: 1 }}
+            className="text-2xl md:text-3xl font-semibold text-blue-500"
+          >
             About Me
-          </h2>
-          <p className="text-sm md:text-base font-medium">
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ type: "spring", duration: 4 }}
+            className="text-sm md:text-base font-medium"
+          >
             Hi, my name is Efe Asieghwu Gift, i am a Frontend web developer,
             React developer, and a Tech influencer. I have honed my skills in
             Web Development and advance i have core understanding of advance UI
             design principles. Here are the major skills i do dominate.
-          </p>
+          </motion.p>
           <div className="flex items-start justify-start gap-5 flex-col md:flex-row md:items-center">
-            <span className="text-3xl md:text-4xl lg:text-6xl font-semibold text-blue-500">
+            <motion.span
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{ type: "spring", duration: 1 }}
+              className="text-3xl md:text-4xl lg:text-6xl font-semibold text-blue-500"
+            >
               4+
-            </span>
-            <span className="text-lg md:text-xl font-medium">
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0, x: -40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{ type: "spring", duration: 1 }}
+              className="text-lg md:text-xl font-medium"
+            >
               Years of experience. Specialised in building web applications,
               while ensuring a seamless
               <br className="hidden lg:block" /> web performance and user
               experience.
-            </span>
+            </motion.span>
           </div>
           <div className="w-full mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             {data.map((item, i) => (
