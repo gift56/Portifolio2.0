c7d0fd2f52fb08c5ec8894e79321875b84834e8f
gift56 <efeasiughu@gmail.com>
2023-12-22 16:56:45 -0500

    working on new update

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -6,53 +6,9 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { projectData } from "@/contant";
 
 const Projects = () => {
-  const data = [
-    {
-      image: "/img/image1.png",
-      title: "Tiktik-World",
-      desc: "A dynamic and engaging environment for users to create, discover, and share short videos.",
-      liveUrl: "https://tiktik-world.vercel.app",
-      repo: "https://github.com/gift56/Tiktik_World",
-    },
-    {
-      image: "/img/image2.png",
-      title: "Zfarmers",
-      desc: "A dynamic website that Empowering farmers through blockchain. Start farming, earn, travel, play and learn with zkFarmers.",
-      liveUrl: "https://zkfarmers.io/",
-      repo: "https://github.com/ShopOasisDapp/zkBuds-Connect",
-    },
-    {
-      image: "/img/image3.png",
-      title: "Ivorytusk",
-      desc: "A dynamic website that promote the adoption of blockchain technology and support the growth of the web3 ecosystem.",
-      liveUrl: "https://ivorytusk.finance/",
-      repo: "https://ivorytusk.finance/",
-    },
-    {
-      image: "/img/image4.png",
-      title: "MovieWorld",
-      desc: "A dynamic website that promote the adoption of blockchain technology and support the growth of the web3 ecosystem.",
-      liveUrl: "https://netflix-clone-roan-nine.vercel.app/",
-      repo: "https://github.com/gift56/Netflix_Clone",
-    },
-    {
-      image: "/img/image5.png",
-      title: "MovieWorld",
-      desc: "A dynamic website that provides the opportunity to Find the perfect freelance services for your business",
-      liveUrl: "https://fiverr-web.netlify.app/",
-      repo: "https://github.com/gift56/Fiverr-clone",
-    },
-    {
-      image: "/img/image6.png",
-      title: "MovieWorld",
-      desc: "A website that promotes Grow with Innovative Product Development, High-End Web Design, and Effective Branding.",
-      liveUrl: "https://colhacklab.com/",
-      repo: "https://colhacklab.com/",
-    },
-  ];
-
   const [screen, setScreen] = useState<number | any>(null);
 
   useEffect(() => {
@@ -106,7 +62,7 @@ const Projects = () => {
               }}
               modules={[Autoplay, Pagination]}
             >
-              {data.map((item) => (
+              {projectData.slice(0, 6).map((item) => (
                 <SwiperSlide
                   key={item.desc}
                   className="w-full p-6 flex flex-col items-start justify-start bg-primary rounded-xl shadow-cardShad md:!h-[450px] mb-10"
---
+++ contant.ts
@@ -0,0 +1,44 @@
+export const projectData = [
+  {
+    image: "/img/image1.png",
+    title: "Tiktik-World",
+    desc: "A dynamic and engaging environment for users to create, discover, and share short videos.",
+    liveUrl: "https://tiktik-world.vercel.app",
+    repo: "https://github.com/gift56/Tiktik_World",
+  },
+  {
+    image: "/img/image2.png",
+    title: "Zfarmers",
+    desc: "A dynamic website that Empowering farmers through blockchain. Start farming, earn, travel, play and learn with zkFarmers.",
+    liveUrl: "https://zkfarmers.io/",
+    repo: "https://github.com/ShopOasisDapp/zkBuds-Connect",
+  },
+  {
+    image: "/img/image3.png",
+    title: "Ivorytusk",
+    desc: "A dynamic website that promote the adoption of blockchain technology and support the growth of the web3 ecosystem.",
+    liveUrl: "https://ivorytusk.finance/",
+    repo: "https://ivorytusk.finance/",
+  },
+  {
+    image: "/img/image4.png",
+    title: "MovieWorld",
+    desc: "A dynamic website that promote the adoption of blockchain technology and support the growth of the web3 ecosystem.",
+    liveUrl: "https://netflix-clone-roan-nine.vercel.app/",
+    repo: "https://github.com/gift56/Netflix_Clone",
+  },
+  {
+    image: "/img/image5.png",
+    title: "MovieWorld",
+    desc: "A dynamic website that provides the opportunity to Find the perfect freelance services for your business",
+    liveUrl: "https://fiverr-web.netlify.app/",
+    repo: "https://github.com/gift56/Fiverr-clone",
+  },
+  {
+    image: "/img/image6.png",
+    title: "MovieWorld",
+    desc: "A website that promotes Grow with Innovative Product Development, High-End Web Design, and Effective Branding.",
+    liveUrl: "https://colhacklab.com/",
+    repo: "https://colhacklab.com/",
+  },
+];
